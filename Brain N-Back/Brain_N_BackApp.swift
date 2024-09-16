d08db9228b92033493e406a4559000181e0678fe
VadimPetroviOS <107857712+VadimPetroviOS@users.noreply.github.com>
2024-09-16 14:30:12 +0300

    initial  commit

---
+++ Brain N-Back/Brain_N_BackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brain_N_BackApp.swift
+//  Brain N-Back
+//
+//  Created by Вадим on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Brain_N_BackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Brain N-Back/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  MainView.swift
+//  Brain N-Back
+//
+//  Created by Вадим on 16.09.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
