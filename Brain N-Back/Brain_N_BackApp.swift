aa9cffa24087d8ef576ed3832b1de8859c0026fc
VadimPetroviOS <107857712+VadimPetroviOS@users.noreply.github.com>
2024-09-17 14:01:00 +0300

    add MainView

--- Brain N-Back/Brain_N_BackApp.swift
+++ Brain N-Back/Brain_N_BackApp.swift
@@ -5,13 +5,19 @@
 //  Created by Вадим on 16.09.2024.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct Brain_N_BackApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView()
+            MainView(
+                store: Store(
+                    initialState: MainReducer.State(),
+                    reducer: { MainReducer() }
+                )
+            )
         }
     }
 }
